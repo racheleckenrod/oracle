6c44cf4e00a669b5d660e30e777bfd264bdf5940
racheleckenrod <rachel@racheleckenrod.com>
2026-09-20 19:33:55 -0400

    Fix CORS url string and restore missing routing layout

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080
 const cors = require('cors')
 
 app.use(cors({
-    origin: 'https://netlify.app'
+    origin: 'https://oracle-reading.netlify.app'
 }));
 
 const cards = {
