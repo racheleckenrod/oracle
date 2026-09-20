64cdfc28e47e88efe130fc171c298de081e9e77a
racheleckenrod <rachel@racheleckenrod.com>
2026-09-20 19:27:09 -0400

    Update CORS configuration with explicit Netlify origin

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 8080
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://netlify.app'
+}));
 
 const cards = {
     'card1':{
